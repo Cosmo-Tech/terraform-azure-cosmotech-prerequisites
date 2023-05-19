d0b11205969383339d0fb539b625d690c2d7e54a
enocquet <emmanuelnocquet8@gmail.com>
2023-05-19 19:04:06 +0200

    output created values for platform sp

---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "out_platform_sp_client_id" {
+  value = module.create-platform-prerequisite.out_platform_clientid
+}
+
+output "out_platform_sp_client_secret" {
+  value = module.create-platform-prerequisite.out_platform_password
+}
