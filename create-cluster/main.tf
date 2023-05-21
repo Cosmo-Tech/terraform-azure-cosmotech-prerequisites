880f9c160eff39563f8f66c740cef5c835470f0b
enocquet <emmanuelnocquet8@gmail.com>
2023-05-21 16:17:46 +0200

    remove acr role assignement

--- create-cluster/main.tf
+++ create-cluster/main.tf
@@ -200,10 +200,3 @@ resource "azurerm_container_registry" "acr" {
   network_rule_bypass_option    = "AzureServices"
   zone_redundancy_enabled       = false
 }
-
-resource "azurerm_role_assignment" "acr-role" {
-  principal_id                     = azurerm_kubernetes_cluster.phoenixcluster.kubelet_identity[0].object_id
-  role_definition_name             = "AcrPull"
-  scope                            = azurerm_container_registry.acr.id
-  skip_service_principal_aad_check = true
-}
