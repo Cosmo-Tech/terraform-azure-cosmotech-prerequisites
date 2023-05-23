6357eaa448efdbf4490f4d45801c711c70f700b6
enocquet <emmanuelnocquet8@gmail.com>
2023-05-23 19:01:12 +0200

    subnet id for default nodepools

--- create-cluster/main.tf
+++ create-cluster/main.tf
@@ -29,6 +29,7 @@ resource "azurerm_kubernetes_cluster" "phoenixcluster" {
     name       = "default"
     node_count = 1
     vm_size    = "Standard_D2_v2"
+    vnet_subnet_id = var.subnet_id
   }
 }
 
