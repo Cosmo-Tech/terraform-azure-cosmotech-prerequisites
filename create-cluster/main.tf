2b043e50c300f56adf5b951bef2d52b00d35926b
enocquet <emmanuelnocquet8@gmail.com>
2023-05-30 11:51:29 +0200

    identity and sp cannot be defined in cluster definition

--- create-cluster/main.tf
+++ create-cluster/main.tf
@@ -10,9 +10,6 @@ resource "azurerm_kubernetes_cluster" "phoenixcluster" {
   kubernetes_version                = var.kubernetes_version
   role_based_access_control_enabled = true
   private_cluster_enabled           = false
-  identity {
-    type = "UserAssigned"
-  }
 
   network_profile {
     load_balancer_sku = "standard"
